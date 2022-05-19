130440abe7e68c9b3d222a6ceb0563bff1602689
John Garbero <john.garbero.itb@gmail.com>
2022-05-19 15:42:24 -0500

    commit tree a87c98f083d7356c1bcf5c39639979dc474f8cbd

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// const e = document.querySelector(main)
+// e.removeChild();
+
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+
+newHeader.textContent = "YOUR-NAME is the champion"
